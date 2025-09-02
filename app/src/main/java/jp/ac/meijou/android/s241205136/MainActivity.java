74a38111ba38ea76023efa9edc62ffaa0da088d4
Rimi Nishida <184547242+innRy@users.noreply.github.com>
2025-09-02 14:53:06 +0900

    データの永続化

--- app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity.java
@@ -16,6 +16,7 @@ import jp.ac.meijou.android.s241205136.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private PrefDataStore prefDataStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +30,11 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        prefDataStore = PrefDataStore.getInstance(this);
+        binding.saveButton.setOnClickListener(view -> {
+            var text = binding.editTextText.getText().toString();
+            prefDataStore.setString("name", text);
         });
         binding.text.setText(R.string.text);
         binding.button.setOnClickListener(view -> {
