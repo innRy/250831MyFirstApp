51282b9670110eba872607aa6e8b90099aebc2be
Rimi Nishida <184547242+innRy@users.noreply.github.com>
2025-09-03 16:20:49 +0900

    レイアウトと画面遷移

--- app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity2.java
@@ -1,24 +1,85 @@
 package jp.ac.meijou.android.s241205136;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Optional;
+
+import jp.ac.meijou.android.s241205136.databinding.ActivityMain2Binding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
+
+    private final ActivityResultLauncher<Intent> getActivityResult = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            result -> {
+                switch (result.getResultCode()) {
+                    case RESULT_OK -> {
+                        Optional.ofNullable(result.getData())
+                                .map(data -> data.getStringExtra("ret"))
+                                .map(text -> "Result: " + text)
+                                .ifPresent(text -> binding.textviewResult.setText(text));
+                    }
+                    case RESULT_CANCELED -> {
+                        binding.textviewResult.setText("Result: Canceled");
+                    }
+                    default -> {
+                        binding.textviewResult.setText("Result: Unknown(" + result.getResultCode() + ")");
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //明示的
+        binding.buttonA.setOnClickListener(v ->{
+            var intent = new Intent(this,MainActivity.class);
+            startActivity(intent);
+        });
+
+        //暗黙的
+        binding.buttonB.setOnClickListener(v ->{
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("https://www.e-typing.ne.jp/"));
+            startActivity(intent);
+        });
+
+        // send button
+        binding.buttonSend.setOnClickListener(v ->{
+            // EditTextの文字列を取得
+            var text = binding.editTextName.getText().toString();
+            var intent = new Intent(this,MainActivity4.class);
+            intent.putExtra("title", text);
+            startActivity(intent);
+        });
+
+        binding.buttonExec.setOnClickListener(v -> {
+            var intent = new Intent(this, MainActivity4.class);
+            getActivityResult.launch(intent);
+        });
     }
+
 }
---
+++ app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity4.java
@@ -0,0 +1,50 @@
+package jp.ac.meijou.android.s241205136;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import jp.ac.meijou.android.s241205136.databinding.ActivityMain4Binding;
+import jp.ac.meijou.android.s241205136.databinding.ActivityMainBinding;
+
+public class MainActivity4 extends AppCompatActivity {
+
+    private jp.ac.meijou.android.s241205136.databinding.ActivityMain4Binding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMain4Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //MainActivity2 から送られたデータを取得
+        var editText = getIntent().getStringExtra("title");
+        binding.textView4.setText(editText);
+
+        // OK button
+        binding.button10.setOnClickListener(v -> {
+            var intent = new Intent();
+            intent.putExtra("ret", "OK");
+            setResult(RESULT_OK, intent);
+            finish();
+        });
+
+        // Cancel button
+        binding.button11.setOnClickListener(v -> {
+            setResult(RESULT_CANCELED);
+            finish();
+        });
+
+    }
+}
