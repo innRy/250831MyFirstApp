4f85b91286ed04775a67d6558eb330a5be959551
Rimi Nishida <184547242+innRy@users.noreply.github.com>
2025-09-04 14:12:51 +0900

    画像取得表示

--- app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity6.java
+++ app/src/main/java/jp/ac/meijou/android/s241205136/MainActivity6.java
@@ -1,5 +1,8 @@
 package jp.ac.meijou.android.s241205136;
 
+import android.app.Activity;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -9,34 +12,31 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.squareup.moshi.Json;
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
 
 import java.io.IOException;
-import java.util.Optional;
 
-import jp.ac.meijou.android.s241205136.databinding.ActivityMain5Binding;
+import jp.ac.meijou.android.s241205136.databinding.ActivityMain6Binding;
 import jp.ac.meijou.android.s241205136.databinding.ActivityMainBinding;
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.OkHttp;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity5 extends AppCompatActivity {
-    private ActivityMain5Binding binding;
+public class MainActivity6 extends AppCompatActivity {
+
+    private ActivityMain6Binding binding;
     private final OkHttpClient okHttpClient = new OkHttpClient();
     private final Moshi moshi = new Moshi.Builder().build();
     private final JsonAdapter<Gist> gistJsonAdapter = moshi.adapter(Gist.class);
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        binding = ActivityMain5Binding.inflate(getLayoutInflater());
+        binding = ActivityMain6Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -44,31 +44,34 @@ public class MainActivity5 extends AppCompatActivity {
             return insets;
         });
 
+        binding.button61.setOnClickListener(view -> {
+            var text = binding.editText1.getText().toString();
+            var url = Uri.parse("https://placehold.jp/f7701a/f99311/500x500.png")
+                    .buildUpon()
+                    .appendQueryParameter("text", text)
+                    .build()
+                    .toString();
+            getImage(url);
+        });
+    }
+    private void getImage(String url){
+        //リクエスト先に画像URLを指定
         var request = new Request.Builder()
-                .url("https://mura.github.io/meijou-android-sample/gist.json")
+                .url(url)
                 .build();
 
         okHttpClient.newCall(request).enqueue(new Callback() {
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                //通信に失敗したときに呼ばれる
             }
 
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
-                var gist = gistJsonAdapter.fromJson(response.body().source());
-                // 中身の取り出し
-                Optional.ofNullable(gist)
-                        .map(g->g.files.get("OkHttp.txt"))
-                        .ifPresent(gistFile -> {
-                            // UIスレッド以外で更新するとクラッシュするので，UIスレッド上で実行させる
-                            runOnUiThread(() -> binding.textView5.setText(gistFile.content));
-                        });
+                var bitmap = BitmapFactory.decodeStream(response.body().byteStream());
+                runOnUiThread(() -> binding.imageView.setImageBitmap(bitmap));
             }
         });
 
-
-
-
     }
-
 }
